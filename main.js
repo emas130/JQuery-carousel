75456d3d8427a68abc26407deefcb842d7cea692
emas130 <pepe130he@wp.pl>
2018-02-04 15:46:08 +0100

    init

---
+++ main.js
@@ -0,0 +1,17 @@
+$(function () {
+   var carouselList = $("#carousel ul");
+   setInterval(changeSlide, 3000);
+
+   function changeSlide() {
+      carouselList.animate( {
+         marginLeft: -400
+      }, 500, placeFirstSlideAfterLast);
+   }
+
+   function placeFirstSlideAfterLast() {
+      carouselList.find("li:last").after(carouselList.find("li:first"));
+      carouselList.css({
+         marginLeft: 0
+      });
+   };
+});
